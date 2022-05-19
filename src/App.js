b3fcbd34ef298082aefdcb493c1cedbbe1ad6dc7
Sergio Ivan Melgarejo <sergio.ivan.melgarejo@gmail.com>
2022-05-19 13:40:53 -0300

    added only bootstrap css + app container

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
+// Library
 import { Route, Routes } from "react-router-dom";
+
+// Components
 import Login from "./components/Login";
 import Listado from "./components/Listado";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+// Style
+import "./css/bootstrap.min.css"
+import "./css/app.css"
+
 function App() {
   return (
-    <>
+    <div className="container">
       <Header/>
       <Routes >
         <Route path="/listado" element={<Listado />} ></Route>
         <Route path="/login" element={<Login />} ></Route>
       </Routes>  
       <Footer />
-    </>
+    </div>
   );
 }
 
