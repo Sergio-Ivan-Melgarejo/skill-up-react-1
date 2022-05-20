62386188977e559b1391ebaa5af1615837a7211a
Sergio Ivan Melgarejo <sergio.ivan.melgarejo@gmail.com>
2022-05-20 16:49:28 -0300

    added movie card

--- src/components/List.js
+++ src/components/List.js
@@ -16,7 +16,18 @@ const List = () => {
  
 
   return (
-    <div className='list'>Listado</div>
+    <>
+      <h2 className='title'>List</h2>
+      <div className='list'>
+        <div className='movie'>
+          <div className='movie__container'>
+            <img className='movie__img' src='' alt=''/>
+            <p className='movie__description'>de d as das d as d as d as d asdsada sd a sd a sd as d as d  as d asd  asd ad</p>
+          </div>
+          <h3 className='movie__title'>title</h3>
+        </div>
+      </div>
+    </>
   )
 }
 
