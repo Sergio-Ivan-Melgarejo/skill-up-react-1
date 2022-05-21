1012146f836429b670e4f3ef1d1a3d40507a8990
Sergio Ivan Melgarejo <sergio.ivan.melgarejo@gmail.com>
2022-05-21 12:36:36 -0300

    styles for destokp navs

--- src/components/Header.js
+++ src/components/Header.js
@@ -55,18 +55,9 @@ function Header () {
 
                 {
                     token 
-
-                    ?   <>
-                            <div onClick={handleOpenConf} className={openConf ? 'nav-item nav-user open' : 'nav-item nav-user'}>
-                                <img className='avatar' src={avatar} alt="avatar"/>
-                            </div>  
-                            <ul className={openConf ? "avatar-conf open" :'avatar-conf'}>
-                                <li className='li'>
-                                    <button className='btn'>Sing out</button>
-                                </li>
-                            </ul>
-                            <div onClick={handleOpenConf} className={openConf ? 'shadow open' : 'shadow'}></div> 
-                        </> 
+                    ?   <div onClick={handleOpenConf} className={openConf ? 'nav-item nav-user open' : 'nav-item nav-user'}>
+                            <img className='avatar' src={avatar} alt="avatar"/>
+                        </div>
                     :   <div className='nav-item nav-user'>
                             <NavLink className="nav-link" to="/login" >
                                 <svg className='svg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z"/></svg>
@@ -75,6 +66,19 @@ function Header () {
                         </div> 
                 }
             </nav>
+            
+            {
+                    token 
+                    ?   <> 
+                            <ul className={openConf ? "avatar-conf open" :'avatar-conf'}>
+                                <li className='li'>
+                                    <button className='btn'>Sing out</button>
+                                </li>
+                            </ul>
+                            <div onClick={handleOpenConf} className={openConf ? 'shadow open' : 'shadow'}></div> 
+                        </> 
+                    :   null
+                }
         </header>
     )
 }
